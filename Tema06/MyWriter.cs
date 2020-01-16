78ca0fe8c8a8512c5dce6e3567023b2bed16f77d
albog <alboghardau@gmail.com>
2020-01-16 23:47:09 +0200

    Homework completed

---
+++ Tema06/MyWriter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tema06
+{
+    class MyWriter
+    {
+        private string filename;
+        private List<string> list;
+
+        public MyWriter(List<string> list, string filename)
+        {
+            this.list = list;
+            this.filename = filename;
+        }
+
+        public void WriteAll()
+        {
+            try
+            {
+                using (StreamWriter sw = File.CreateText(filename))
+                {
+                    this.list.ForEach(line => sw.WriteLine(line.ToString()));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }        
+    }
+}
--- Tema06/Program.cs
+++ Tema06/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Tema06
 {
@@ -10,6 +11,56 @@ namespace Tema06
     {
         static void Main(string[] args)
         {
+            List<string> goodNumbers = new List<string>();
+            List<string> badNumbers = new List<string>();
+            string[] file;
+
+            string filename = "numbers.txt";
+
+            if (File.Exists(filename))
+            {
+                file = File.ReadAllLines(filename);
+
+                foreach (string line in file)
+                {
+                    if (TryParse(line))
+                    {
+                        goodNumbers.Add(line);
+                    }
+                    else
+                    {
+                        badNumbers.Add(line);
+                    }
+                }
+
+                Console.WriteLine("GOOD NUMBERS:");
+                goodNumbers.ForEach(number => Console.WriteLine(number.ToString()));
+                Console.WriteLine("BAD NUMBERS:");
+                badNumbers.ForEach(number => Console.WriteLine(number.ToString()));
+
+                MyWriter goodBoy = new MyWriter(goodNumbers, "correctNumbers.txt");
+                goodBoy.WriteAll();
+
+                MyWriter badBoy = new MyWriter(badNumbers, "incorrectNumbers.txt");
+                badBoy.WriteAll();
+            }
+            else
+            {
+                Console.WriteLine("Input file not found!");
+            }
         }
+
+        private static Boolean TryParse(string line)
+        {
+            try
+            {
+                int.Parse(line);
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
+
     }
 }
